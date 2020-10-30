816719c4bc5b0167042c4f702749b05eae099e2c
seanhughes530 <seanhughes530@gmail.com>
2020-10-29 21:06:35 -0400

    Basic outline with a shit ton of errors. Need to get Main.swift to refer to VisionObjectRecognitionViewController.swift

---
+++ Recyclopedia/Main.swift
@@ -0,0 +1,22 @@
+//
+//  Main.swift
+//  Recyclopedia
+//
+//  Created by Sean Hughes on 10/29/20.
+//  Copyright © 2020 Sean Hughes. All rights reserved.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text("What up, Bitches")
+        // need to get VisionObjectRecognitionViewController here
+    }
+}
+
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Main()
+    }
+}
